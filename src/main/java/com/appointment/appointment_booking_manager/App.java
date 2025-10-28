71c7e3c8166929765f227084724f8f4de35fdf1f
Hrishi <hrishigaa@gmail.com>
2025-10-28 20:00:26 +0100

    Initial pom.xml with all dependencies and plugins

---
+++ src/main/java/com/appointment/appointment_booking_manager/App.java
@@ -0,0 +1,13 @@
+package com.appointment.appointment_booking_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
