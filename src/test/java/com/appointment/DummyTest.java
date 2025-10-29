e9dcf1f7f0c3db2674d1b9bbc44f6e79ad2b5640
Hrishi <hrishigaa@gmail.com>
2025-10-29 18:18:56 +0100

    Add Dummy test to verifyCI pipeline

---
+++ src/test/java/com/appointment/DummyTest.java
@@ -0,0 +1,10 @@
+package com.appointment;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class DummyTest {
+	void testDummy() {
+		assertTrue(true);
+	}
+}
