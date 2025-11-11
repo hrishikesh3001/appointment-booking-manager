d6a60ddeec16de384e0cb8b207940e9e59b35af9
Hrishi <hrishigaa@gmail.com>
2025-11-11 17:25:12 +0100

    Removed MySQL, controller and config files(following from the book)

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS appointment (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    appointment_date DATETIME NOT NULL,
-    service_type VARCHAR(255) NOT NULL,
-    status VARCHAR(50) NOT NULL
-);
